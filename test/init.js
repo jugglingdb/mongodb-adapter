16deb78dda3c8b0ce364b9c7a351a8ff1f6744e5
Anatoliy Chakkaev <mail@anatoliy.in>
2016-07-10 17:13:46 +0100

    Update deps

--- test/init.js
+++ test/init.js
@@ -4,7 +4,7 @@ var Schema = require('jugglingdb').Schema;
 
 global.getSchema = function() {
     var db = new Schema(require('../'), {
-        url: 'mongodb://travis:test@localhost:27017/myapps'
+        url: process.env.MONGO_URL || 'mongodb://travis:test@localhost:27017/myapps'
     });
     db.log = function (a) { console.log(a); };
 
